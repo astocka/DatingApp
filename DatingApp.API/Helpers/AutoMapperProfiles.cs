4a5337db49575c761f88921e8540f82b0eca3ae7
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2018-11-09 12:18:19 +0100

    End of section 11

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -29,6 +29,8 @@ namespace DatingApp.API.Helpers
                  });
             CreateMap<Photo, PhotosForDetailsDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
